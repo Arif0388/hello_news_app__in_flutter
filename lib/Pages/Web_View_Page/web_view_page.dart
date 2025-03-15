aa55bf8c25e517aa6f8ab4b6ef8c64dea5854dbb
Arif0388 <arifhussai542@gmail.com>
2025-03-15 19:12:15 +0530

    add web view page and his functionality

--- lib/Pages/Web_View_Page/web_view_page.dart
+++ lib/Pages/Web_View_Page/web_view_page.dart
@@ -1,10 +1,43 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewPage extends StatelessWidget {
+class WebViewPage extends StatefulWidget {
   const WebViewPage({super.key, required this.blogUrl});
  final String blogUrl;
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late WebViewController controller;
+  var loadingPercentage = 0;
+  @override
+  void initState(){
+    super.initState();
+    controller = WebViewController()
+    ..setNavigationDelegate(NavigationDelegate(
+      onPageStarted:(url){
+         setState(() {
+           loadingPercentage = 0;
+         });
+      },
+      onProgress:(progress){
+        setState(() {
+           loadingPercentage = progress;
+        });
+      },
+      onPageFinished:(url){
+        setState(() {
+          loadingPercentage = 100;
+        });
+      }
+    ))
+    ..loadRequest(Uri.parse(widget.blogUrl));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,9 +46,13 @@ class WebViewPage extends StatelessWidget {
         title:Text('Article View Page!',style:Theme.of(context).textTheme.headlineSmall,),
         elevation:1,
       ),
-      body:WebView(
-        initialUrl:blogUrl,
-        javascriptMode: JavascriptMode.unrestricted,
+      body:Stack(
+        children:[
+          WebViewWidget(
+            controller:controller,
+          ),
+          loadingPercentage < 100 ? const Center(child: CupertinoActivityIndicator(color:Colors.greenAccent)) : Container(),
+        ]
       ),
     );
   }
